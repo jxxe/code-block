cf93db77e5ea2b6afa53649a8784d0a7a089a5e6
Jerome Paulos <jeromepaulos@yahoo.com>
2020-10-13 21:24:06 -0700

    Create plugin file

---
+++ code-block.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: Code Block
+Plugin URI: https://github.com/jxxe/code-block
+Author: Jerome Paulos
+Author URI: https://jeromepaulos.com
+Description: A Gutenberg block for displaying code with syntax highlighting.
+*/
